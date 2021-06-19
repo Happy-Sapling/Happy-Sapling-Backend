9458f6a93763409af0aa7637840a57c4f2cb1676
Jeannie Q <jeanniequach01@gmail.com>
2021-06-19 10:58:08 -0400

    connecting mongodb

---
+++ server/db/index.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const connection =
+  "mongodb+srv://HSAdmin:ElKcKLYKI7ELqqW0@hs-cluster.rxjry.mongodb.net/hs-cluster?retryWrites=true&w=majority";
+
+mongoose
+  .connect(connection, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Database connected succesfully"))
+  .catch((e) => {
+    console.error("Connection error", e.message);
+  });
+
+const db = mongoose.connection;
+
+module.exports = db;
--- server/index.js
+++ server/index.js
@@ -2,7 +2,17 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const apiPort = 3000;
+const apiPort = 9000;
+
+const db = require("./db");
+
+/* var corsOptions = {
+  origin: "http://localhost:9001/",
+};
+ */
+//app.use(cors(corsOptions));
+app.use(express.json());
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
