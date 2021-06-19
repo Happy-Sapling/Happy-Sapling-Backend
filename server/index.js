2466f63eaf514117a4773d66181ac8ea87fb8756
Jeannie Q <jeanniequach01@gmail.com>
2021-06-19 10:04:44 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,18 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const apiPort = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World!");
 });
+
+app.get("/api/users", (req, res) => {
+  res.send([1, 2, 3]);
+});
+
+app.get("/api/posts/:year/:month", (req, res) => {
+  res.send(req.query);
+});
+
+app.listen(apiPort, () => console.log(`listening on port ${apiPort}...`));
