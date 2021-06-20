c100faab1fcef44b171fad93356008027a1a349d
Jeannie Q <jeanniequach01@gmail.com>
2021-06-19 20:01:50 -0400

    updating index + email

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
-const connection = "";
+const connection =
+  "mongodb+srv://HSAdmin:ElKcKLYKI7ELqqW0@hs-cluster.rxjry.mongodb.net/hs-cluster?retryWrites=true&w=majority";
 
 mongoose
   .connect(connection, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Database connected successfully"))
+  .then(() => console.log("Database Connected Succesfully"))
   .catch((e) => {
     console.error("Connection error", e.message);
   });
+
+//used to solve deprecation warnings
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 
 const db = mongoose.connection;
 
