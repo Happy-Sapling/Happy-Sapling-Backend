041ef9f993c2c7141c446553e09afbccb607d291
Jeannie Q <jeanniequach01@gmail.com>
2021-06-20 03:08:28 -0400

    changes

--- server/models/journal-model.js
+++ server/models/journal-model.js
@@ -1,15 +1,3 @@
-/* const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const Reason = new Schema(
-  {
-    reason: { type: String, required: true },
-  },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model("Reasons", Reason); */
-
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
